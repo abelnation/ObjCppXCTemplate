1c728e89626c2721b2c9892193d15329910c0491
aallison <aallison@yahoo-inc.com>
2014-06-07 01:39:50 -0700

    initial commit

---
+++ Objective C++/Objective C++ Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___FULLUSERNAME___. All rights reserved.
+//
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+public:
+  ___FILEBASENAMEASIDENTIFIER___();
+  
+private:
+
+}
